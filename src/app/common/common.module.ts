62195417490bcdc449729fa20daa6dd31bcd7598
Frank <50495924+Bae26@users.noreply.github.com>
2021-10-13 21:43:46 -0700

    added search bar

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SearchBarComponent],
   imports: [CommonModule],
   exports: [CommonModule, HeaderComponent],
 })
